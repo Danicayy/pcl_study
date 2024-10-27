60d94d25a9e65c70185d1a5dfb63aa2677d5fbb5
Danicayy <149891497+Danicayy@users.noreply.github.com>
2024-10-27 22:49:28 +0800

    add

---
+++ dataload/build/CMakeFiles/dataload.dir/DependInfo.cmake
@@ -0,0 +1,61 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/danica/dataload/dataload.cpp" "/home/danica/dataload/build/CMakeFiles/dataload.dir/dataload.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_ATOMIC_DYN_LINK"
+  "BOOST_ATOMIC_NO_LIB"
+  "BOOST_FILESYSTEM_DYN_LINK"
+  "BOOST_FILESYSTEM_NO_LIB"
+  "BOOST_IOSTREAMS_DYN_LINK"
+  "BOOST_IOSTREAMS_NO_LIB"
+  "BOOST_SERIALIZATION_DYN_LINK"
+  "BOOST_SERIALIZATION_NO_LIB"
+  "BOOST_SYSTEM_DYN_LINK"
+  "BOOST_SYSTEM_NO_LIB"
+  "EIGEN_HAS_CXX17_OVERALIGN=0"
+  "H5_BUILT_AS_DYNAMIC_LIB"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_SQL_LIB"
+  "QT_WIDGETS_LIB"
+  "qh_QHpointer"
+  "vtkDomainsChemistry_AUTOINIT=1(vtkDomainsChemistryOpenGL2)"
+  "vtkIOExport_AUTOINIT=2(vtkIOExportOpenGL2,vtkIOExportPDF)"
+  "vtkRenderingContext2D_AUTOINIT=1(vtkRenderingContextOpenGL2)"
+  "vtkRenderingCore_AUTOINIT=3(vtkInteractionStyle,vtkRenderingFreeType,vtkRenderingOpenGL2)"
+  "vtkRenderingOpenGL2_AUTOINIT=1(vtkRenderingGL2PSOpenGL2)"
+  "vtkRenderingVolume_AUTOINIT=1(vtkRenderingVolumeOpenGL2)"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/pcl-1.14"
+  "/usr/include/eigen3"
+  "/usr/local/include/vtk-8.2"
+  "/home/danica/anaconda3/include"
+  "/usr/include/ni"
+  "/usr/include/openni2"
+  "/home/danica/anaconda3/include/qt"
+  "/home/danica/anaconda3/include/qt/QtWidgets"
+  "/home/danica/anaconda3/include/qt/QtGui"
+  "/home/danica/anaconda3/include/qt/QtCore"
+  "/home/danica/anaconda3/./mkspecs/linux-g++"
+  "/home/danica/anaconda3/include/qt/QtSql"
+  "/usr/local/include/vtk-8.2/vtkogg/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ dataload/build/CMakeFiles/dataload.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dataload.dir/dataload.cpp.o"
+  "dataload"
+  "dataload.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dataload.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
